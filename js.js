aba93ee0225027d9acee12bb9d13fb8afdb20011
Hauke <92950300+demhauke@users.noreply.github.com>
2021-10-23 01:43:59 +0200

    Update js.js

--- js.js
+++ js.js
@@ -1,5 +1,6 @@
 
 function copy_discord() {
     document.getElementById("button_id").innerHTML = 'kopiert!';
-    navigator.clipboard.writeText('dem Hauke#5791')
+    navigator.clipboard.writeText('dem Hauke#5791');
+    setTimeout("location.reload(true);", time);
 }
