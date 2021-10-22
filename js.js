443e1c99e2975c19f35639d49f77884e479a0513
Hauke <92950300+demhauke@users.noreply.github.com>
2021-10-23 01:03:34 +0200

    Add files via upload

---
+++ js.js
@@ -0,0 +1,15 @@
+let copied_text = false;
+
+function timeRefresh(time) {
+    setTimeout("location.reload(true);", time);
+}
+
+function copy_discord() {
+    var copy_text = 'dem Hauke#5791'
+    navigator.clipboard.writeText(copy_text)
+    let copied_text = true;
+}
+
+if (copied_text == true){
+    document
+}
