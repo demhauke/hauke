6c8c42f6bc0def6919b5307f840b07449dc12098
Hauke <92950300+demhauke@users.noreply.github.com>
2021-11-04 15:18:19 +0100

    Add files via upload

--- js.js
+++ js.js
@@ -3,12 +3,27 @@
 //    setTimeout("location.reload(true);", time);
 //    }
 
-function change_discord() {
-    document.getElementById("button_id").innerHTML = 'Discord';
+function reload() {
+    setTimeout("location.reload")
 }
 
+
+function change_discord() {
+    document.getElementById("d_button_id").innerHTML = 'Discord';
+}
+function change_nintendo() {
+    document.getElementById("n_button_id").innerHTML = 'Nintendo';
+}
+
+
 function copy_discord() {
-    document.getElementById("button_id").innerHTML = 'kopiert!';
+    document.getElementById("d_button_id").innerHTML = 'kopiert!';
     navigator.clipboard.writeText('dem Hauke#5791');
     setTimeout(change_discord, 800)  
+}
+
+function copy_nintendo() {
+    document.getElementById("n_button_id").innerHTML = 'kopiert!';
+    navigator.clipboard.writeText('SW-8087-1043-0728');
+    setTimeout(change_nintendo, 800)  
 }
